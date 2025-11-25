f6e8305c7f05f6754f02c14b325f717e3cc55326
Shtrecker <gogels235@gmail.com>
2025-11-25 22:20:59 +0200

    Add backpack animator

--- src/xrGame/ActorInput.cpp
+++ src/xrGame/ActorInput.cpp
@@ -852,7 +852,7 @@ bool CActor::use_Holder(CHolderCustom* holder)
 
 void CActor::ActorUse()
 {
-	if (HudAnimator() && HudAnimator()->ItemAnimator()->IsActive())
+	if (HudAnimator() && HudAnimator()->IsAnyAnimatorActive())
 	{
 		return;
 	}
